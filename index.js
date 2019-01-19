fb38bc623be1e61742c8dcfd341686ee945092de
kkuyu <devils67@naver.com>
2019-01-19 22:53:03 +0900

    handle profile add

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`)
 }
 
+function handleHome(req, res){
+    //req: request object
+    //res: response object
+    res.send("Hello form home")
+}
+
+function handleProfile(req, res){
+    res.send("My Profile")
+}
+
+app.get("/", handleHome)
+
+app.get("/profile", handleProfile)
+
 app.listen(PORT, handleListening);

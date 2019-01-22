649031f2a392a50a5b29eafba0000145905b90d6
kkuyu <devils67@naver.com>
2019-01-22 22:25:08 +0900

    url router division

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {userRouter} from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
-
-//req: request object
-//res: response object
-const handleHome = (req, res) => res.send("Hello form home");
-const handleProfile = (req, res) => res.send("My Profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -18,9 +15,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter)
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;

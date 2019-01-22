f435f29921af37fb0da207478e7eb5db481fee6c
kkuyu <devils67@naver.com>
2019-01-22 18:02:18 +0900

    cookie-parser, body-parser add

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -13,6 +15,9 @@ const handleListening = () => console.log(`Listening on: http://localhost:${PORT
 const handleHome = (req, res) => res.send("Hello form home");
 const handleProfile = (req, res) => res.send("My Profile");
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 

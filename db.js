c5237da718676e3967d64075524b99ff32181180
kkuyu <devils67@naver.com>
2019-01-23 23:25:42 +0900

    mixins video block, home each  item

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title: "Video Title Test",
         description: "test id 123456 hahaha",
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content%2Fbig_buck_bunny_720p_surround.mp4?cnt=0",
         creater: {
             id: 456789,
             userName: "archive",
@@ -16,7 +16,7 @@ export const videos = [
         title: "Video Title Test2",
         description: "test id 147852 hahaha",
         views: 78,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content%2Fbig_buck_bunny_720p_surround.mp4?cnt=0",
         creater: {
             id: 569854,
             userName: "archive",
@@ -28,7 +28,7 @@ export const videos = [
         title: "Video Title Test3",
         description: "test id 369852 hahaha",
         views: 15,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content%2Fbig_buck_bunny_720p_surround.mp4?cnt=0",
         creater: {
             id: 256314,
             userName: "archive",
@@ -40,7 +40,7 @@ export const videos = [
         title: "Video Title Test4",
         description: "test id 987456 hahaha",
         views: 94,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content%2Fbig_buck_bunny_720p_surround.mp4?cnt=0",
         creater: {
             id: 589647,
             userName: "archive",
@@ -52,7 +52,7 @@ export const videos = [
         title: "Video Title Test5",
         description: "test id 951753 hahaha",
         views: 65,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content%2Fbig_buck_bunny_720p_surround.mp4?cnt=0",
         creater: {
             id: 145236,
             userName: "archive",

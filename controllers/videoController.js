7ba8ab9fdc69fffa05c49bf7292f994f866c5c30
kkuyu <devils67@naver.com>
2019-02-11 17:23:16 +0900

    getJoin, postJoin

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,7 +4,7 @@ export const home = (req, res) => res.render("home", { pageTitle: "Home", videos
 export const search = (req, res) => {
 	// const searchingBy = req.query.term;
 	const {query: {term : searchingBy } } = req;
-	res.render("search", { pageTitle: "Search", searchingBy: searchingBy });
+	res.render("search", { pageTitle: "Search", searchingBy: searchingBy, videos: videos });
 }
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });

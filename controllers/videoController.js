4bded366760ffc4fb39d2143d942220ff9a262ab
kkuyu <devils67@naver.com>
2019-03-04 14:50:30 +0900

    video id

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -22,7 +22,6 @@ export const postUpload = async(req, res) => {
 		body: {title, description},
 		file: {path}
 	} = req;
-	console.log(title, description)
 	const newVideo = await Video.create({
 		fileUrl: path,
 		title,
@@ -31,6 +30,16 @@ export const postUpload = async(req, res) => {
 	res.redirect(routes.videoDetail(newVideo.id));
 }  
 
-export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetail = async(req, res) => {
+	const {
+		params: {id}
+	} = req;
+	try{
+		const video = await Video.findById(id);
+		res.render("videoDetail", { pageTitle: "Video Detail", video: video });
+	}catch(error){
+		res.redirect(routes.home)
+	}
+}
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });

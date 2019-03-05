c352b5d6f2866b80ac7261c8563f86fd6f91fc77
kkuyu <devils67@naver.com>
2019-03-05 10:41:14 +0900

    delete video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -36,7 +36,7 @@ export const videoDetail = async(req, res) => {
 	} = req;
 	try{
 		const video = await Video.findById(id);
-		res.render("videoDetail", { pageTitle: "Video Detail", video: video });
+		res.render("videoDetail", { pageTitle: video.title, video: video });
 	}catch(error){
 		res.redirect(routes.home)
 	}
@@ -66,4 +66,12 @@ export const postEditVideo = async(req, res) => {
 	}
 }
 
-export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = async(req, res) => {
+	const {
+		params: {id}
+	} = req;
+	try{
+		await Video.findOneAndRemove({ _id: id });
+	}catch(error){}
+	res.redirect(routes.home);
+}

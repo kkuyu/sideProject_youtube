fc10fcba2d051036c989aff553c27201fb12c676
kkuyu <devils67@naver.com>
2019-03-07 11:44:12 +0900

    style added

--- middlewares.js
+++ middlewares.js
@@ -7,7 +7,7 @@ export const localMiddlewere = (req, res, next) => {
     res.locals.siteName = "Side Project, Cloning Youtube";
     res.locals.routes = routes;
     res.locals.user = {
-        isAuthenticated: true,
+        isAuthenticated: false,
         id: 1
     }
     next();

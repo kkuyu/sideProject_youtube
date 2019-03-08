2597cca57399dca3d615b1523d0a939759640abc
kkuyu <devils67@naver.com>
2019-03-08 17:01:58 +0900

    onlyPublic, onlyPrivate

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import mongoose from "mongoose";
 import session from "express-session";
-
+import MongoStore from "connect-mongo";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -15,6 +16,8 @@ import { localMiddlewere } from "./middlewares";
 import "./passport"
 
 const app = express();
+
+const CookieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -30,7 +33,8 @@ app.use(
 	session({
 		secret: process.env.COOKIE_SECRET,
 		resave: true,
-		saveUninitialized: false
+		saveUninitialized: false,
+		store: new CookieStore({ mongooseConnection: mongoose.connection })
 	})
 );
 app.use(passport.initialize());
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -2,14 +2,15 @@ import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
 import { getJoin, postJoin, getLogin, postLogin, logout } from "../controllers/userController";
+import { onlyPublic } from "../middlewares";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin, postLogin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, postJoin, onlyPublic, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);

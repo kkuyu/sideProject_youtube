6f057c621316483b7896188976c84eb9cf6f5035
kkuyu <devils67@naver.com>
2019-03-14 16:23:21 +0900

    recoad video save

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,17 +3,31 @@ const recordBtn = document.getElementById("jsRecordButton");
 const videoPreview = document.getElementById("jsVideoPreview");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = event => {
-  console.log(event);
+	const { data: videoFile } = event;
+	const link = document.createElement("a");
+	link.href = URL.createObjectURL(videoFile);
+	link.download = "recored.webm";
+	document.body.appendChild(link);
+	link.click();
+};
+
+const stopRecording = () => {
+	videoRecorder.stop();
+	recordBtn.removeEventListener("click", stopRecording);
+	recordBtn.addEventListener("click", getVideo);
+	recordBtn.innerHTML = "Start recording";
 };
 
 const startRecording = () => {
-	const videoRecorder = new MediaRecorder(streamObject);
+	videoRecorder = new MediaRecorder(streamObject);
 	videoRecorder.start();
 	videoRecorder.addEventListener("dataavailable", handleVideoData);
+	recordBtn.addEventListener("click", stopRecording);
 };
-  
+
 const getVideo = async () => {
 	try{
 		const stream = await navigator.mediaDevices.getUserMedia({

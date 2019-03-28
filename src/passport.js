c6a5f5979f710f716603b620dc2e6f9545e87c7f
kkuyu <devils67@naver.com>
2019-03-28 11:13:03 +0900

    passport url change

--- src/passport.js
+++ src/passport.js
@@ -11,7 +11,7 @@ passport.use(
 	new GithubStrategy({
 		clientID: process.env.GH_ID,
 		clientSecret: process.env.GH_SECRET,
-		callbackURL: `http://localhost:4000${routes.githubCallback}`
+		callbackURL: process.env.PRODUCTION ? `https://secret-wave-84195.herokuapp.com${routes.githubCallback}` : `http://localhost:4000${routes.githubCallback}`
 	}, githubLoginCallback)
 );
 
@@ -19,7 +19,7 @@ passport.use(
 	new FacebookStrategy({
 		clientID: process.env.FB_ID,
 		clientSecret: process.env.FB_SECRET,
-		callbackURL: `https://0bdb9b3d.ngrok.io${routes.facebookCallback}`,
+		callbackURL: process.env.PRODUCTION ? `https://secret-wave-84195.herokuapp.com${routes.facebookCallback}` : `http://localhost:4000${routes.facebookCallback}`,
 		profileFields: [ "id", "displayName", "photos", "email" ],
 		scope: [ "public_profile", "email" ]
 	}, facebookLoginCallback)

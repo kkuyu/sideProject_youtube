e335e05b4f0a2c90c965b75bef2b5780641874d5
kkuyu <devils67@naver.com>
2019-03-28 11:28:51 +0900

    loggedUser change

--- src/middlewares.js
+++ src/middlewares.js
@@ -30,7 +30,7 @@ export const uploadAvatar = multerAvatar.single('avatar');
 export const localMiddlewere = (req, res, next) => {
     res.locals.siteName = "Side Project, Cloning Youtube";
     res.locals.routes = routes;
-    res.locals.loggedUser = req.user || null;
+    res.locals.loggedUser = req.user || {};
     next();
 }
 
